7f7087c66be3c9c4dd6d8ea63e56a01acb90c337
Abdelrahman Yehia <54885581+A-lang209@users.noreply.github.com>
2025-06-24 03:28:43 +0300

    Create Nutrition Facts.py

---
+++ Nutrition Facts.py
@@ -0,0 +1,27 @@
+fruits: dict[str, int]= {
+    "apple":           130,
+    "banana":          110,
+    "pear":            100,
+    "sweet cherries":  100,
+    "grapes":           90,
+    "kiwifruit":        90,
+    "orange":           80,
+    "watermelon":       80,
+    "plums":            70,
+    "grapefruit":       60,
+    "nectarine":        60,
+    "peach":            60,
+    "avocado":          50,
+    "cantaloupe":       50,
+    "honeydew melon":   50,
+    "pineapple":        50,
+    "strawberries":     50,
+    "tangerine":        50,
+    "lime":             20,
+    "lemon":            15,
+}
+
+
+fruit = input("Itme: ").lower()
+if fruit in fruits:
+    print("Calories: ", fruits[fruit])
